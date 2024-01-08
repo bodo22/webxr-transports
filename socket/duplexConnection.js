b1aa951b0caf8d6225076d4aae31ddde226e7b52
Wilhelm Herbrich <wilhelm.herbrich@st.ovgu.de>
2024-01-08 15:36:37 +0100

    fix: cache access race condition

--- socket/duplexConnection.js
+++ socket/duplexConnection.js
@@ -13,8 +13,6 @@ const cache = Cache.default({
   // ns: "my-namespace", // Optional. A grouping namespace for items.
 });
 
-console.log(cache.getSync("key"));
-
 const parserMap = {
   default: "",
   msgpack: msgpackParser,
@@ -27,18 +25,32 @@ export default class DuplexConnection {
     this.app = express();
     this.port = options.port;
     if (this.options.secure) {
-      this.server = https.createServer({
-        key: cache.getSync("key"),
-        cert: cache.getSync("cert"),
-      });
+      this.waitForVite = setInterval(() => {
+        const cert = cache.getSync("cert");
+        const key = cache.getSync("key");
+        console.log("getting cert & key");
+        if (cert && key) {
+          console.log("found cert & key");
+          this.server = https.createServer({
+            key,
+            cert,
+          });
+          clearInterval(this.waitForVite);
+          this.waitForVite = undefined;
+          this.initialize();
+        }
+      }, 1000);
     } else {
       this.server = http.createServer(this.app);
+      this.initialize();
     }
     this.app.use(cors());
     this.pingPongsPerSec = [];
     this.pingPong = 0;
     this.sockets = {};
+  }
 
+  initialize() {
     this.server.on("upgrade", (_, socket) => {
       socket.on("close", () => {
         const { bytesRead, bytesWritten } = socket;
@@ -49,7 +61,7 @@ export default class DuplexConnection {
 
     this.server.listen(this.port, () => {
       console.log(
-        `socket server ${options.parser} listening at :${
+        `socket server ${this.options.parser} listening at :${
           this.port
         }, https: ${!!this.options.secure}`
       );
@@ -66,9 +78,11 @@ export default class DuplexConnection {
       config.parser = this.parser;
     }
     this.io = new Server(this.server, config);
-    this.io.of("/").on("connection", options.onConnect.bind(this));
-    if (options.onAdminConnect) {
-      this.io.of("/admin").on("connection", options.onAdminConnect.bind(this));
+    this.io.of("/").on("connection", this.options.onConnect.bind(this));
+    if (this.options.onAdminConnect) {
+      this.io
+        .of("/admin")
+        .on("connection", this.options.onAdminConnect.bind(this));
     }
   }
 }
